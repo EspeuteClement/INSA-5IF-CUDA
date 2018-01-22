a847b91f2f533e02af27d5b14fa667cbc852a2c4
Sn0wFox <ruben.pericas@gmail.com>
2018-01-22 14:43:33 +0100

    Clean project 1

--- 1/cuda_td_1.cu
+++ 1/cuda_td_1.cu
@@ -17,31 +17,30 @@
 
 
 __global__ void vecAdd(float *in1, float *in2, float *out, int len) {
-	int i = blockIdx.x * blockDim.x + threadIdx.x;
-	printf("AM DOING STUFF");
+  int i = blockIdx.x * blockDim.x + threadIdx.x;
 
-	if (i < len)
-	{
-		out[i] = in1[i] + in2[i];
-	}
+  if (i < len)
+  {
+    out[i] = in1[i] + in2[i];
+  }
 }
 
 int main(int argc, char **argv) {
-    wbArg_t args;
-    int inputLength;
-    float *hostInput1;
-    float *hostInput2;
-	float *hostOutput;
-    args = wbArg_read(argc, argv);
-    wbTime_start(Generic, "Importing data and creating memory on host");
-    hostInput1 =
-    (float *)wbImport(wbArg_getInputFile(args, 0), &inputLength);
-    hostInput2 =
-    (float *)wbImport(wbArg_getInputFile(args, 1), &inputLength);
-    hostOutput = (float *)malloc(inputLength * sizeof(float));
-    wbTime_stop(Generic, "Importing data and creating memory on host");
-    wbLog(TRACE, "The input length is ", inputLength);
-    wbTime_start(GPU, "Allocating GPU memory.");
+  wbArg_t args;
+  int inputLength;
+  float *hostInput1;
+  float *hostInput2;
+  float *hostOutput;
+  args = wbArg_read(argc, argv);
+  wbTime_start(Generic, "Importing data and creating memory on host");
+  hostInput1 =
+  (float *)wbImport(wbArg_getInputFile(args, 0), &inputLength);
+  hostInput2 =
+  (float *)wbImport(wbArg_getInputFile(args, 1), &inputLength);
+  hostOutput = (float *)malloc(inputLength * sizeof(float));
+  wbTime_stop(Generic, "Importing data and creating memory on host");
+  wbLog(TRACE, "The input length is ", inputLength);
+  wbTime_start(GPU, "Allocating GPU memory.");
 	
 	float * memInput1 = nullptr;
 	cudaMalloc(&memInput1, inputLength * sizeof(float));
@@ -52,45 +51,41 @@ int main(int argc, char **argv) {
 	float * memOutput = nullptr;
 	cudaMalloc(&memOutput, inputLength * sizeof(float));
 
-    wbTime_stop(GPU, "Allocating GPU memory.");
-    wbTime_start(GPU, "Copying input memory to the GPU.");
+  wbTime_stop(GPU, "Allocating GPU memory.");
+  wbTime_start(GPU, "Copying input memory to the GPU.");
 
-	cudaMemcpy(memInput1, hostInput1, inputLength * sizeof(float), cudaMemcpyKind::cudaMemcpyHostToDevice);
-	cudaMemcpy(memInput2, hostInput2, inputLength * sizeof(float), cudaMemcpyKind::cudaMemcpyHostToDevice);
-	//cudaMemset(memOutput, 0, inputLength * sizeof(float));
+  cudaMemcpy(memInput1, hostInput1, inputLength * sizeof(float), cudaMemcpyKind::cudaMemcpyHostToDevice);
+  cudaMemcpy(memInput2, hostInput2, inputLength * sizeof(float), cudaMemcpyKind::cudaMemcpyHostToDevice);
+  //cudaMemset(memOutput, 0, inputLength * sizeof(float));
 
-	//@@ Copy memory to the GPU here
-    wbTime_stop(GPU, "Copying input memory to the GPU.");
-    //@@ Initialize the grid and block dimensions here
+  //@@ Copy memory to the GPU here
+  wbTime_stop(GPU, "Copying input memory to the GPU.");
+  //@@ Initialize the grid and block dimensions here
 	
-	int blockSize(256);
-	int gridSize((inputLength-1)/blockSize + 1);
+  int blockSize(256);
+  int gridSize((inputLength-1)/blockSize + 1);
 
-    wbTime_start(Compute, "Performing CUDA computation");
-	
+  wbTime_start(Compute, "Performing CUDA computation");
 
+  vecAdd KERNEL_ARGS2(gridSize, blockSize) (memInput1, memInput2, memOutput, inputLength);
 
-	vecAdd KERNEL_ARGS2(gridSize, blockSize) (memInput1, memInput2, memOutput, inputLength);
-
-    cudaDeviceSynchronize();
-    wbTime_stop(Compute, "Performing CUDA computation");
-    wbTime_start(Copy, "Copying output memory to the CPU");
+  cudaDeviceSynchronize();
+  wbTime_stop(Compute, "Performing CUDA computation");
+  wbTime_start(Copy, "Copying output memory to the CPU");
 
 	cudaMemcpy(hostOutput, memOutput, inputLength * sizeof(float), cudaMemcpyKind::cudaMemcpyDeviceToHost);
-    //@@ Copy the GPU memory back to the CPU here
-    wbTime_stop(Copy, "Copying output memory to the CPU");
-    wbTime_start(GPU, "Freeing GPU Memory");
+  //@@ Copy the GPU memory back to the CPU here
+  wbTime_stop(Copy, "Copying output memory to the CPU");
+  wbTime_start(GPU, "Freeing GPU Memory");
 	cudaFree(memInput1);
 	cudaFree(memInput2);
 	cudaFree(memOutput);
-    wbTime_stop(GPU, "Freeing GPU Memory");
+  wbTime_stop(GPU, "Freeing GPU Memory");
 
-    wbSolution(args, hostOutput, inputLength);
-    free(hostInput1);
-    free(hostInput2);
-    free(hostOutput);
+  wbSolution(args, hostOutput, inputLength);
+  free(hostInput1);
+  free(hostInput2);
+  free(hostOutput);
 
-	getchar();
-
-    return 0;
+  return 0;
 }
